8c5f001b6e1f423d7cfd9de5c8121c55c1a880cc
saisoni96 <131359616+saisoni96@users.noreply.github.com>
2023-08-16 15:50:09 +0530

    Update cors error

--- tryksol/settings.py
+++ tryksol/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'quizapp',
+    'corsheaders',
 ]
 
 
@@ -54,6 +55,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -155,3 +157,4 @@ JAZZMIN_SETTINGS = {
                         "show_sidebar": True,
                         "navigation_expanded": True,
                     }
+CORS_ALLOW_ALL_ORIGINS=True
