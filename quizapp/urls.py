1311aec31b3c79ca78cbf381687e0594d63f79e5
saisoni96 <131359616+saisoni96@users.noreply.github.com>
2023-10-27 20:43:12 +0530

    Update urls.py

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import SendOTPView,VerifyOTPView, LogoutView,UserRegisterView,UserLoginView,ResetPasswdView, CategoryListView, quizListView, Results,FetchNewQuiz,SaveQuizResponse,ResultDetails
+from .views import SendOTPView,VerifyOTPView, UserProfileView, CheckUserView, LogoutView,UserRegisterView,UserLoginView,ResetPasswdView, CategoryListView, quizListView, Results,FetchNewQuiz,SaveQuizResponse,ResultDetails
 
 urlpatterns = [
     path('registration/', UserRegisterView.as_view(), name='registeration'),
     path('sendOTP/', SendOTPView.as_view(), name='send-otp'),
     path('verifyOTP/', VerifyOTPView.as_view(), name='verify-otp'),
     path('login/', UserLoginView.as_view(), name='registeration'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('check-user/', CheckUserView.as_view(), name='check-user'),
     path('reset/password/', ResetPasswdView.as_view(), name='reset-password'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('categories/list/', CategoryListView.as_view(), name='categories-list'),
