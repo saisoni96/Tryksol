a9d02dc63164d66535fdc2bab157771746616713
saisoni96 <131359616+saisoni96@users.noreply.github.com>
2023-12-03 14:56:49 +0530

    Update serializers.py

--- quizapp/serializers.py
+++ quizapp/serializers.py
@@ -10,21 +10,18 @@ class UserRegisterSerializer(serializers.Serializer):
     dob = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=255)
     mobile_number = serializers.CharField(max_length=20)  # Add mobile_number field
-
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
             raise serializers.ValidationError('This email is already registered.')
         return value
-
     def validate_mobile_number(self, value):
         if not User.objects.filter(mobile_number=value).exists():
             raise serializers.ValidationError('This mobile number is not registered.')
         return value
-    
     def create(self, validated_data):
         mobile_number = validated_data['mobile_number']
         try:
-            user = User.objects.get(mobile_number=mobile_number)
+            user = User.objects.get(username=str(mobile_number))
             user.email = validated_data['email']
             user.first_name = validated_data['first_name']
             user.last_name = validated_data['last_name']
